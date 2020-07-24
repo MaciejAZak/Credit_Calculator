ca6d2313ae6d85f4ea0945313b33f3607ad68393
Zh00c3q <maciej.a.zak@gmail.com>
2020-07-24 22:56:57 +0200

    finish tasks from module 4/4 without final project

---
+++ Problems/A mean of n/task.py
@@ -0,0 +1,8 @@
+n = int(input())
+r = 0
+
+for n in range(1, n + 1):
+    number = int(input())
+    r = r + number
+
+print(r/n)
---
+++ Problems/Lucky 7/task.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+for n in range(1, n + 1):
+    number = int(input())
+    if number % 7 == 0:
+        print(number ** 2)
